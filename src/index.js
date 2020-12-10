0bfaed94e93a8f80602d9fccf6de3aa45f341e32
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2020-12-09 21:46:54 -0500

    module 4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './main-page';
 import reportWebVitals from './reportWebVitals';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
